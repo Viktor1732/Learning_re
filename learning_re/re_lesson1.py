de6bccfb4bbc2f2bb1aff201ade2c403e24f556a
Viktor1732 <99543565+Viktor1732@users.noreply.github.com>
2022-07-05 13:18:06 +0700

    Закончил тему регулярные выражения

--- learning_re/re_lesson1.py
+++ learning_re/re_lesson1.py
@@ -1,6 +1,54 @@
-import re
+# import re
 
-text = "век, веко, вектор, верификация"
-match = re.findall('век', text)
+# text = "век, веко, вектор"
+# match = re.findall(r'век', text)
+#
+# text = "век, Век, века"
+# match = re.findall(r'[Вв]ек', text)
 
-print(match)
+# text = "век, Век, века"
+# match = re.findall(r'([Вв]ек)', text)
+
+# text = "(век), Век, века"
+# match = re.findall(r'\([Вв]ек\)', text)
+
+# text = "в12ек, Ве7к, ве3ка"
+# match = re.findall('[0123456789][0123456789]', text)
+
+# text = "в12ек, Ве7к, ве3ка"
+# match = re.findall('[0-9][0-9]', text)
+
+# text = "в12ек, Ве-7к, ве3ка"
+# match = re.findall('[-0-9][0-9]', text)
+
+# text = "в12ек, Ве7к, ве3ка"
+# match = re.findall('[^0-9]', text)
+
+# text = "в12ек, Ве7к, ве3ка"
+# match = re.findall('[а-яА-Я]', text)
+
+# text = "в12ек, Ве7к, ве3ка"
+# match = re.findall('[а-яА-Я]', text)
+
+# . - лубой символ
+# \d - любая цифра
+# \D - любая не цира
+# \s - любой пробельный символ(\t\n\f\r\v)
+# \S - любой не пробельный символ
+# \w - любой символ слова. При флаге re.ASCII набору символов [a-zA-Z0-9]
+# \W - любой не символ слова. При флаге re.ASCII набору символов [^a-zA-Z0-9]
+
+# text = "в12ек, Ве7к, ве3ка"
+# match = re.findall(r'\d', text)
+
+# text = "в12ек, Ве7к, ве3ка"
+# match = re.findall(r'\S', text)
+
+# text = "в12ек, Ве7к, ве3ка"
+# match = re.findall(r'\w', text, re.ASCII)
+
+# text = "0xf, 0xa, 0x5"
+# match = re.findall(r"0x[\da-fA-F]", text)
+
+
+# print(match)
