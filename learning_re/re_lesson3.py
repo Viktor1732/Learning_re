36ec4ba8dc3fef9957c49efd38f4f730b14769bf
Viktor1732 <programmer.kras@gmail.com>
2022-07-07 21:46:37 +0700

    Закончил тему "Регулярные выражения"

--- learning_re/re_lesson3.py
+++ learning_re/re_lesson3.py
@@ -21,11 +21,25 @@ import re
 # match = re.findall(r"(<img)\s+[^>]*src*=(?P<qqq>[\"'])(.+?)(?P=qqq)", text)
 # print(match)
 
+# with open("p_course.xml", 'r') as f:
+#     lon = []
+#     lat = []
+#     for text in f:
+#         match = re.findall(r"<point\s*[^>]*lon=([\"\'])(\d.*)\1\s*lat=\1(\d.*)\1", text)
+#         if len(match) > 0:
+#             lon.append(match[0][1])
+#             lat.append(match[0][2])
+#     print(lon, lat, sep="\n")
+
 with open("p_course.xml", 'r') as f:
-    lat = []
     lon = []
+    lat = []
     for text in f:
+        match = re.search(r"<point\s*[^>]*lon=([\"\'])(?P<lon>\d.*)\1\s*lat=\1(?P<lat>\d.*)\1", text)
+        if match:
+            v = match.groupdict()
+            if "lon" in v and "lat" in v:
+                lon.append(v["lon"])
+                lat.append(v["lat"])
+    print(lon, lat, sep="\n")
 
-
-        match = re.findall(r"<point\s+[^>]*?lon=([\"\'])([0-9.,]+)\1\s+[^>]*lat=([\"\'])([0-9.,]+)\1>", text)
-    print(lon, lat, sep='\n')
